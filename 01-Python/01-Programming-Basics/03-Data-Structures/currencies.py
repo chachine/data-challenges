c8aa6da52b9c3f0ab9512e383dbf86815d47d679
Yassine El Abbassi <yassine@elabbassi.org>
2022-08-31 16:43:12 +0100

    data structure currencies

--- 01-Python/01-Programming-Basics/03-Data-Structures/currencies.py
+++ 01-Python/01-Programming-Basics/03-Data-Structures/currencies.py
@@ -1,11 +1,18 @@
 # pylint: disable=missing-docstring
 
 # TODO: add some currency rates
-RATES = {}
+RATES = {"USDEUR" : 0.85,
+         "GBPEUR": 1.132,
+         "CHFEUR": 0.86,
+         "EURGBP": 0.885}
 
 def convert(amount, currency):
     """returns the converted amount in the given currency
     amount is a tuple like (100, "EUR")
     currency is a string
     """
-    pass  # YOUR CODE HERE
+    pair = amount[1] + currency
+    if pair in RATES.keys():
+        return int( amount[0] * RATES[pair])
+    else:
+        return None
