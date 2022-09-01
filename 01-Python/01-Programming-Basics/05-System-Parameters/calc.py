0cd535321a618b0733276f6deaae4964953a57d5
Yassine El Abbassi <yassine@elabbassi.org>
2022-09-01 12:30:05 +0100

    sys argv

--- 01-Python/01-Programming-Basics/05-System-Parameters/calc.py
+++ 01-Python/01-Programming-Basics/05-System-Parameters/calc.py
@@ -1,11 +1,24 @@
 # pylint: disable=missing-module-docstring,missing-function-docstring,eval-used
 import sys
 
+import operator
 
 def main():
     """Implement the calculator"""
-    pass  # YOUR CODE HERE
 
+
+    ops = {
+        '+': operator.add,
+        '-': operator.sub,
+        '*': operator.mul,
+        '/': operator.truediv,  # use operator.div for Python 2
+        '%': operator.mod,
+        '^': operator.xor,
+    }
+    # print(ops[sys.argv[2]])
+    # result = sys.argv[1] + ops[sys.argv[2]] + sys.argv[3]
+    result = ops[sys.argv[2]](int(sys.argv[1]), int(sys.argv[3]))
+    return result
 
 if __name__ == "__main__":
     print(main())
