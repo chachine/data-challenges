0f35d523e8b763e8aca70d0b0a6ec11feafd675d
Yassine El Abbassi <yassine@elabbassi.org>
2022-09-01 16:15:48 +0100

    csv challenges

--- 01-Python/02-Data-Sourcing/01-CSV/phone_book.py
+++ 01-Python/02-Data-Sourcing/01-CSV/phone_book.py
@@ -1,1 +1,7 @@
+import csv
 
+with open('data/phone_book.csv', mode='r') as csv_file:
+    reader = csv.DictReader(csv_file)
+    for row in reader:
+        print(row["last_name"]+': '+row["phone_number"])
+       
