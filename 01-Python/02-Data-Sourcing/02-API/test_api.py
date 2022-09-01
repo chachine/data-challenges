805e140defad786fc7527f097c26efe61aa37930
Yassine El Abbassi <yassine@elabbassi.org>
2022-09-01 17:18:03 +0100

    api from correction

--- 01-Python/02-Data-Sourcing/02-API/test_api.py
+++ 01-Python/02-Data-Sourcing/02-API/test_api.py
@@ -1,4 +1,17 @@
-# pylint: disable=missing-docstring,invalid-name
+import requests
 
-# TODO: paste the code from Kitt's instructions
+url = "https://weather.lewagon.com/geo/1.0/direct?q=Barcelona"
+response = requests.get(url).json()
+city = response[0]
 
+print(f"{city['name']}: ({city['lat']}, {city['lon']})")
+lat = city['lat']
+lon = city['lon']
+# url_forecast = f'https://weather.lewagon.com/data/2.5/forecast/daily?lat={lat}&lon={lon}&cnt=5'
+url_forecast = f'https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&units=metric&appid=54633ebdb5fee724d4fab9f87561640b'
+response_forecast = requests.get(url_forecast).json()
+
+for i in range(0,5):
+    print(response_forecast["list"][i]["dt_txt"])
+    print(response_forecast["list"][i]["weather"][0]["description"])
+    print(response_forecast["list"][i]["main"]["temp"])
--- 01-Python/02-Data-Sourcing/02-API/weather.py
+++ 01-Python/02-Data-Sourcing/02-API/weather.py
@@ -11,18 +11,45 @@ def search_city(query):
     '''Look for a given city. If multiple options are returned, have the user choose between them.
        Return one city (or None)
     '''
-    pass  # YOUR CODE HERE
+    # $CHALLENGIFY_BEGIN
+    url = urllib.parse.urljoin(BASE_URI, "/geo/1.0/direct")
+    cities = requests.get(url, params={'q': query}).json()
+    if not cities:
+        print(f"Sorry, OpenWeather does not know about {query}...")
+        return None
+    if len(cities) == 1:
+        return cities[0]
+    for i, city in enumerate(cities):
+        print(f"{i + 1}. {city['title']}")
+    index = int(input("Oops, which one did you mean?")) - 1
+    return cities[index]
+    # $CHALLENGIFY_END
+
 
 def weather_forecast(lat, lon):
     '''Return a 5-day weather forecast for the city, given its latitude and longitude.'''
-    pass  # YOUR CODE HERE
+    # $CHALLENGIFY_BEGIN
+    url = urllib.parse.urljoin(BASE_URI, "/data/2.5/forecast")
+    forecasts = requests.get(
+        url, params={'lat': lat, 'lon': lon, 'units': 'metric'}).json()['list']
+    return forecasts[::8]
+    # $CHALLENGIFY_END
+
 
 def main():
     '''Ask user for a city and display weather forecast'''
     query = input("City?\n> ")
     city = search_city(query)
     # TODO: Display weather forecast for a given city
-    pass  # YOUR CODE HERE
+    # $CHALLENGIFY_BEGIN
+    if city:
+        daily_forecasts = weather_forecast(city['lat'], city['lon'])
+        for forecast in daily_forecasts:
+            max_temp = round(forecast['main']['temp_max'])
+            print(
+                f"{forecast['dt_txt'][:10]}: {forecast['weather'][0]['main']} ({max_temp}°C)")
+    # $CHALLENGIFY_END
+
 
 if __name__ == '__main__':
     try:
